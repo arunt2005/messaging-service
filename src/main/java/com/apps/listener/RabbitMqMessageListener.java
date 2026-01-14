64782dba21d65619548c7fe8b1868676c9c7b327
arunt2005 <arunt2005@gmail.com>
2026-01-14 22:34:03 +0530

    fix

--- src/main/java/com/apps/listener/RabbitMqMessageListener.java
+++ src/main/java/com/apps/listener/RabbitMqMessageListener.java
@@ -1,7 +1,9 @@
 package com.apps.listener;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,6 +13,11 @@ public class RabbitMqMessageListener {
     @RabbitListener(queues = "${messaging.queue}")
     public void onMessage(String message) {
         System.out.println("📩 RabbitMQ received message: " + message);
+    }
+
+    @Bean
+    public Queue testQueue() {
+        return new Queue("test.queue", true);
     }
 }
 
